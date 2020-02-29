a68472e5ad896ad4fe2be8c108250b3cfb9f1cde
Jesse Alama <jesse@lisp.sh>
2020-02-29 07:33:06 +0100

    Add test for slightly malformed locale.

--- koyo-test/koyo/l10n.rkt
+++ koyo-test/koyo/l10n.rkt
@@ -21,6 +21,7 @@
       (check-equal? (language-header->locale "") #f)
       (check-equal? (language-header->locale "en") #f)
       (check-equal? (language-header->locale "en-US") '(en . us))
+      (check-equal? (language-header->locale "en-USA") #f)
       (check-equal? (language-header->locale "en, en-US;q=0.5, ro-RO;q=1") '(ro . ro))
       (check-equal? (language-header->locale "ro, en-GB;q=0.3, en-US;q=0.1, ro-RO;q=0.5") '(ro . ro))))
 
